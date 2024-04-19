18ee9316d30511e274c10c74e3ddf514f5b71722
creih <iherr.9@gmail.com>
2024-04-19 06:33:40 -0700

    task 0 and readme file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# this puppet file creates a school file inside tmp dir
+file { 'tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
