85b667c63b033a5afedac13fadeaced107ba3b0e
creih <iherr.9@gmail.com>
2024-04-19 06:52:09 -0700

    added a / for home before tmp/school

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-# this puppet file creates a school file inside tmp dir
-file { 'tmp/school':
+file { '/tmp/school':
   ensure  => file,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }
