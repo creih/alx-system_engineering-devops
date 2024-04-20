d22520b4fdc5605639b945984c45acb9a1380634
creih <iherr.9@gmail.com>
2024-04-20 05:19:50 -0700

    installed werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,11 @@
-# task 1 kwenstalla flask
-exec { 'update_flask':
-  command  => '/usr/bin/pip3 install --upgrade flask',
-  unless   => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  provider => 'shell',
+# task 1 kwenstalla werkzeug
+package { 'werkzeug':
+  ensure    => '2.1.1',
+  provider  => 'pip3',
 }
 
 # Install Flask version 2.1.0
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Exec['update_flask'],
 }
