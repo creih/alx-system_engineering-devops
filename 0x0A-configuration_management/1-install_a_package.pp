7f48d1706cb16c387d7d276ff7771766b8e87b92
creih <iherr.9@gmail.com>
2024-04-20 02:28:16 -0700

    added a script to update flask dep

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
 # task 1 kwenstalla flask
+exec { 'update_flask':
+  command  => '/usr/bin/pip3 install --upgrade flask',
+  unless   => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  provider => 'shell',
+}
+
+# Install Flask version 2.1.0
 package { 'flask':
-  ensure   => 'installed',
-  provider => 'pip',
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Exec['update_flask'],
 }
