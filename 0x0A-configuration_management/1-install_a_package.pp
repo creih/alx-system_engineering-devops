aba1fe0233f175b250e813c2a3f0e51518ffe143
creih <iherr.9@gmail.com>
2024-04-20 05:23:41 -0700

    aligned the arrow

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # task 1 kwenstalla werkzeug
 package { 'werkzeug':
-  ensure    => '2.1.1',
-  provider  => 'pip3',
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
 
 # Install Flask version 2.1.0
