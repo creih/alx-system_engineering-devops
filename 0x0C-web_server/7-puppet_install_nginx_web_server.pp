a89378615225aa88a3bad3ee0b10099546af8a18
creih <iherr.9@gmail.com>
2024-04-23 13:04:39 -0700

    task 7 made

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,34 @@
+# Define the nginx class
+class { 'nginx':
+  ensure       => 'installed',
+  manage_repo  => true,
+}
+
+# Configure Nginx to listen on port 80
+nginx::resource::vhost { 'example.com':
+  ensure        => present,
+  listen_port   => '80',
+  proxy         => 'http://localhost:8080', # Proxy to handle redirects
+  proxy_set_header => ['Host $host', 'X-Real-IP $remote_addr', 'X-Forwarded-For $proxy_add_x_forwarded_for', 'X-Forwarded-Proto $scheme'],
+}
+
+# Define the redirection resource
+nginx::resource::location { '/redirect_me':
+  ensure      => present,
+  vhost       => 'example.com',
+  location    => '^/redirect_me',
+  return      => '301 https://example.com/new_location',
+}
+
+# Define a custom index page
+file { '/usr/share/nginx/html/index.html':
+  ensure  => present,
+  content => "Hello World!\n",
+}
+
+# Restart Nginx service after configuration changes
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => [File['/usr/share/nginx/html/index.html'], Nginx::Resource::Location['/redirect_me']],
+}
