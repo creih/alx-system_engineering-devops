72647886adf329f4f2a9449fd5e36d638f17af50
creih <iherr.9@gmail.com>
2024-05-20 14:40:29 -0700

    task 0 retrial

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,34 +1,24 @@
 #!/usr/bin/python3
 """
-This module ni iya task 0 muri 0x15-API project and is displaying
-a user name and a set of completed tasks.
+task 0 iri kuri 0x15-API iri ku displayinga employe na tasks yakoze
 """
 import requests
 import sys
 
+
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
+
     em_id = sys.argv[1]
     usr_r = requests.get(url + "users/{}".format(em_id))
     usr = usr_r.json()
     par = {"userId": em_id}
     tods_r = requests.get(url + "todos", params=par)
-    tod = tods_r.json()
-    izarangiye = []
-    for tos in tod:
-        if tos.get("completed") is True:
-            izarangiye.append(tos.get("title"))
-    
-    output = "Employee {} is done with tasks ({}/{}):".format(
-        usr.get("name"),
-        len(izarangiye),
-        len(tod)
-    )
-    with open("student_output", "w") as f:
-        f.write(output + "\n")
-        for combo in izarangiye:
-            f.write("\t {}\n".format(combo))
-
-    print(output)
-    for combo in izarangiye:
-        print("\t {}".format(combo))
+    tods = tods_r.json()
+    rangiye = []
+    for tod in tods:
+        if tod.get("completed") is True:
+            rangiye.append(tod.get("title"))
+    print("Employee {} is done with tasks({}/{}):".format(usr.get("name"), len(rangiye), len(tod)))
+    for rangiy in rangiye:
+        print("\t {}".format(rangiy))
