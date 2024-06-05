50ad2bee036af29daff33dcdc6ce6a635e255f57
creih <iherr.9@gmail.com>
2024-06-05 19:38:32 +0000

    task1 n readme file

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,14 @@
+file { '/etc/apache2/sites-available/000-default.conf':
+  ensure  => file,
+  source  => 'puppet:///modules/apache/000-default.conf',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+  require => Package['apache2'],
+}
+
+service { 'apache2':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/apache2/sites-available/000-default.conf'],
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+repo dir for web debuging
