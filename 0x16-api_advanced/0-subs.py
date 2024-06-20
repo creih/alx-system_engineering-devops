cb6b603046fc7382d3af7e55c9624e56bb8d4318
creih <iherr.9@gmail.com>
2024-06-20 20:29:05 +0200

    fixed the checks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,8 @@ def number_of_subscribers(subreddit):
     """ tasks' function as requested in the task."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'my-app'}
-    
+
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
